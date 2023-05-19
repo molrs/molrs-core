e16ef60c7bc69b9cadbd254d86d7f848f2e04cf4
Sam Mun <therealsam@berkeley.edu>
2023-05-19 16:58:30 -0700

    unique chains
    add code for finding unique chains and generating coordinates for them

--- src/molecule.rs
+++ src/molecule.rs
@@ -337,43 +337,144 @@ impl Molecule {
                 }
             }
         }
-        for chain in chains.iter_mut() {
+        let mut chains = deduplicate_nested_vec(&chains, "");
+        chains.sort_by_key(|chain| chain.len().wrapping_neg());
+
+        let mut unique_chains = vec![chains[0].clone()];
+        let mut unique_atoms: HashSet<usize> = HashSet::from_iter(chains[0].iter().copied());
+        for chain in chains[1..].iter_mut() {
+            let mut unique_chain = vec![];
+            if !unique_atoms.contains(chain.iter().next().unwrap()) {
+                for index in chain.iter() {
+                    unique_chain.push(*index);
+                    if unique_atoms.contains(index) {
+                        break;
+                    }
+                    unique_atoms.insert(*index);
+                }
+            }
+            if !unique_chain.is_empty() {
+                unique_chains.push(unique_chain);
+            }
+            chain.reverse();
+            let mut unique_chain = vec![];
+            if !unique_atoms.contains(chain.iter().next().unwrap()) {
+                for index in chain.iter() {
+                    unique_chain.push(*index);
+                    if unique_atoms.contains(index) {
+                        break;
+                    }
+                    unique_atoms.insert(*index);
+                }
+            }
+            if !unique_chain.is_empty() {
+                unique_chains.push(unique_chain);
+            }
+        }
+
+        for chain in unique_chains.iter_mut() {
             if chain.first().unwrap() > chain.last().unwrap() {
                 chain.reverse();
             }
         }
-        let mut chains = deduplicate_nested_vec(&chains, "");
-        chains.sort_by_key(|chain| chain.len().wrapping_neg());
 
-        chains
+        unique_chains
     }
 
     pub fn coordinates_2d(&self) -> Vec<Option<[f64; 2]>> {
-        let mut _unique_rings = self.unique_rings();
-        let mut _unique_chains = self.unique_chains();
+        let unique_rings = self.unique_rings();
+        let mut unique_chains = self.unique_chains();
+        unique_chains.sort_by_key(|chain| chain.len());
 
-        let mut _coords: Vec<Option<[f64; 2]>> =
+        let mut coords: Vec<Option<[f64; 2]>> =
             (0..self.graph.node_count()).map(|_| None).collect();
 
+        if unique_chains.is_empty() && unique_rings.is_empty() {
+            coords[0] = Some([0.0, 0.0]);
+        } else if unique_rings.is_empty() {
+            let chain = &unique_chains[0];
+            coords[chain[0]] = Some([0.0, 0.0]);
+            let chain_coords = chain_to_coords(chain.len(), [0.0, 0.0], [0.0, 1.0]);
+            for (index, coord) in chain.iter().skip(1).zip(chain_coords) {
+                coords[*index] = Some(coord);
+            }
+
+            while coords.contains(&None) {}
+        } else {
+        }
+
         // coords.iter().map(|coord| coord.unwrap()).collect()
-        _coords
+        coords
     }
+}
+
+fn chain_to_coords(chain_len: usize, root_coord: [f64; 2], next_coord: [f64; 2]) -> Vec<[f64; 2]> {
+    let next_coord = [next_coord[0] - root_coord[0], next_coord[1] - root_coord[1]];
+    let angle;
+    if next_coord[0] >= 0.0 && next_coord[1] >= 0.0 {
+        angle = (next_coord[1] / next_coord[0]).atan(); // radians
+    } else if next_coord[0] < 0.0 && next_coord[1] >= 0.0 {
+        angle = std::f64::consts::PI - (next_coord[1] / next_coord[0]).atan(); // radians
+    } else if next_coord[0] < 0.0 && next_coord[1] < 0.0 {
+        angle = std::f64::consts::PI + (next_coord[1] / next_coord[0]).atan(); // radians
+    } else {
+        angle = std::f64::consts::TAU - (next_coord[1] / next_coord[0]).atan(); // radians
+    }
+    let alt_angle = angle + std::f64::consts::FRAC_PI_3; // radians
+    let growth_vector = [angle.cos(), angle.sin()];
+    let alt_growth_vector = [alt_angle.cos(), alt_angle.sin()];
+    let mut coords = vec![root_coord];
+    for i in 1..(chain_len) {
+        let last_coord = coords.iter().last().unwrap();
+        if i % 2 == 0 {
+            coords.push([
+                last_coord[0] + alt_growth_vector[0],
+                last_coord[1] + alt_growth_vector[1],
+            ]);
+        } else {
+            coords.push([
+                last_coord[0] + growth_vector[0],
+                last_coord[1] + growth_vector[1],
+            ]);
+        }
+    }
+
+    coords[1..].to_vec()
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
+    fn test_coordinates_2d() {
+        let smiles = ["C", "CC"];
+        let coords_vec = [vec![[0.0, 0.0]], vec![[0.0, 0.0], [0.0, 1.0]]];
+        for (smi, coords) in smiles.iter().copied().zip(coords_vec) {
+            let mol_coords = crate::from::smiles(smi)
+                .unwrap()
+                .coordinates_2d()
+                .iter()
+                .map(|coord| coord.unwrap())
+                .collect::<Vec<[f64; 2]>>();
+            for (coord, mol_coord) in coords.iter().zip(mol_coords) {
+                for i in 0..2 {
+                    assert!(coord[i] - mol_coord[i] <= 0.01);
+                }
+            }
+        }
+    }
+
+    #[flaky_test::flaky_test]
     fn test_unique_chains() {
         let smiles = [
             "CCC",
-            "CC(C)C",
+            "CCC(C)C",
             "C1CC1-C2CC2",
             "C1CC1-C-C2CC2",
             "C1CC1(-C)-C2CC2",
         ];
         let unique_chains_vec = [
             vec![vec![0, 1, 2]],
-            vec![vec![0, 1, 2], vec![0, 1, 3], vec![2, 1, 3]],
+            vec![vec![0, 1, 2, 4], vec![2, 3]],
             vec![vec![2, 3]],
             vec![vec![2, 3, 4]],
             vec![vec![2, 3], vec![2, 4]],
@@ -388,17 +489,5 @@ mod tests {
                 assert!(unique_chains.contains(chain))
             }
         }
-    }
-
-    #[test]
-    fn test_coordinates_2d() {
-        // let smi = "CCC";
-        // let smi = "C(C)C";
-        let smi = "CCCCC(C)CC";
-        // let smi = "c12ncccc1[nH]cc2";
-        // let smi = "C1(CC2)CC2CCC1";
-        let mol = crate::from::smiles(smi).unwrap();
-        // dbg!(&mol);
-        dbg!(mol.coordinates_2d());
     }
 }
